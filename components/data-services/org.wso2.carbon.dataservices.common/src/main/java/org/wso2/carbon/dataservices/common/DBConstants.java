8c3330cad8536ed39a57370e849f14f780b05be1
chanikag <chanika@wso2.com>
2020-06-04 17:19:25 +0530

    Fix EI-5133: Introduce a property to hide current params in the DSFault msg
    This is to fix wso2/product-ei#5133
    
    If the current params should be hidden in the log file and then in the wsdl, the following system property should be enabled.
    dss.disable.current.params=true

--- components/data-services/org.wso2.carbon.dataservices.common/src/main/java/org/wso2/carbon/dataservices/common/DBConstants.java
+++ components/data-services/org.wso2.carbon.dataservices.common/src/main/java/org/wso2/carbon/dataservices/common/DBConstants.java
@@ -84,7 +84,8 @@ public final class DBConstants {
     public static final String SQL_DRIVER_CLASS_NAME = "org.wso2.carbon.dataservices.sql.driver.TDriver";
     public static final String SECURITY_MODULE_NAME = "rampart";
     public static final String TENANT_IN_ONLY_MESSAGE = "TENANT_IN_ONLY_MESSAGE";
-        
+    public static final String DISABLE_CURRENT_PARAMS_IN_LOG = "dss.disable.current.params";
+
     /**
      * Codes to be used as fault codes.
      */

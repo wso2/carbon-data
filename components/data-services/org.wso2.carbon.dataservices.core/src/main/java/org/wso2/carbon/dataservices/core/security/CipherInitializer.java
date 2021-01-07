69b3cec57e25e2ae53e8649ce5e0b97ea8742f77
sajiniekavindya <sajinie.14@cse.mrt.ac.lk>
2021-01-07 10:14:51 +0530

    Add vault-lookup support to dataservices

---
+++ components/data-services/org.wso2.carbon.dataservices.core/src/main/java/org/wso2/carbon/dataservices/core/security/CipherInitializer.java
@@ -0,0 +1,272 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+package org.wso2.carbon.dataservices.core.security;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.securevault.CipherFactory;
+import org.wso2.securevault.CipherOperationMode;
+import org.wso2.securevault.DecryptionProvider;
+import org.wso2.securevault.EncodingType;
+import org.wso2.securevault.commons.MiscellaneousUtil;
+import org.wso2.securevault.definition.CipherInformation;
+import org.wso2.securevault.definition.IdentityKeyStoreInformation;
+import org.wso2.securevault.definition.KeyStoreInformationFactory;
+import org.wso2.securevault.definition.TrustKeyStoreInformation;
+import org.wso2.securevault.keystore.IdentityKeyStoreWrapper;
+import org.wso2.securevault.keystore.KeyStoreWrapper;
+import org.wso2.securevault.keystore.TrustKeyStoreWrapper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class CipherInitializer {
+    private static Log log = LogFactory.getLog(CipherInitializer.class);
+
+    private static CipherInitializer cipherInitializer = new CipherInitializer();
+    private DecryptionProvider decryptionProvider = null;
+    private IdentityKeyStoreWrapper identityKeyStoreWrapper;
+    private TrustKeyStoreWrapper trustKeyStoreWrapper;
+
+    public static CipherInitializer getInstance() {
+        return cipherInitializer;
+    }
+
+    private CipherInitializer() {
+        super();
+        boolean initPro = init();
+        if (initPro) {
+            // initialize the cipher decryption provider for decryption purposes.
+            initCipherDecryptProvider();
+        } else {
+            log.error("Either Configuration properties can not be loaded or No secret"
+                              + " repositories have been configured please check PRODUCT_HOME/repository/conf/security "
+                              + " refer links related to configure WSO2 Secure vault");
+        }
+    }
+
+    private boolean init() {
+        Properties properties = loadProperties();
+
+        if (properties == null) {
+            log.error("KeyStore configuration properties cannot be found");
+            return false;
+        }
+
+        String configurationFile = MiscellaneousUtil.getProperty(properties,
+                                                                 SecureVaultConstants.SECRET_MANAGER_CONF_PROPERTY,
+                                                                 SecureVaultConstants.DEFAULT_CONF_LOCATION_PROPERTY);
+
+        Properties configurationProperties = MiscellaneousUtil.loadProperties(configurationFile);
+        if (configurationProperties.isEmpty()) {
+            if (log.isDebugEnabled()) {
+                log.debug("Configuration properties can not be loaded from : " + configurationFile +
+                                  " Will use synapse properties");
+            }
+            configurationProperties = properties;
+        }
+
+        String globalSecretProvider =
+                MiscellaneousUtil.getProperty(configurationProperties, SecureVaultConstants.SECRET_PROVIDER_PROPERTY,
+                                              null);
+        if (globalSecretProvider == null || "".equals(globalSecretProvider)) {
+            if (log.isDebugEnabled()) {
+                log.debug("No global secret provider is configured.");
+            }
+        }
+
+        String repositoriesString = MiscellaneousUtil.getProperty(configurationProperties,
+                                                                  SecureVaultConstants.SECRET_REPOSITORIES_PROPERTY,
+                                                                  null);
+        if (repositoriesString == null || "".equals(repositoriesString)) {
+            log.error("No secret repositories have been configured");
+            return false;
+        }
+
+        String[] repositories = repositoriesString.split(",");
+        if (repositories.length == 0) {
+            log.error("No secret repositories have been configured");
+            return false;
+        }
+
+        // Create a KeyStore Information for private key entry KeyStore
+        IdentityKeyStoreInformation identityInformation =
+                KeyStoreInformationFactory.createIdentityKeyStoreInformation(properties);
+
+        // Create a KeyStore Information for trusted certificate KeyStore
+        TrustKeyStoreInformation trustInformation =
+                KeyStoreInformationFactory.createTrustKeyStoreInformation(properties);
+
+        String identityKeyPass = null;
+        String identityStorePass = null;
+        String trustStorePass = null;
+        if (identityInformation != null) {
+            identityKeyPass = identityInformation.getKeyPasswordProvider().getResolvedSecret();
+            identityStorePass = identityInformation.getKeyStorePasswordProvider().getResolvedSecret();
+        }
+
+        if (trustInformation != null) {
+            trustStorePass = trustInformation.getKeyStorePasswordProvider().getResolvedSecret();
+        }
+
+        if (!validatePasswords(identityStorePass, identityKeyPass, trustStorePass)) {
+            log.error("Either Identity or Trust keystore password is mandatory"
+                              + " in order to initialized secret manager.");
+            return false;
+        }
+
+        identityKeyStoreWrapper = new IdentityKeyStoreWrapper();
+        identityKeyStoreWrapper.init(identityInformation, identityKeyPass);
+
+        trustKeyStoreWrapper = new TrustKeyStoreWrapper();
+        if (trustInformation != null) {
+            trustKeyStoreWrapper.init(trustInformation);
+        }
+
+        for (String secretRepo : repositories) {
+
+            String sb = SecureVaultConstants.SECRET_REPOSITORIES_PROPERTY + SecureVaultConstants.DOT + secretRepo
+                    + SecureVaultConstants.DOT + SecureVaultConstants.PROVIDER_PROPERTY;
+            String provider = MiscellaneousUtil.getProperty(configurationProperties, sb, null);
+            if (provider == null || "".equals(provider)) {
+                handleException("Repository provider cannot be null ");
+            }
+
+            if (log.isDebugEnabled()) {
+                log.debug("Initiating a File Based Secret Repository");
+            }
+        }
+        return true;
+    }
+
+    private boolean validatePasswords(String identityStorePass, String identityKeyPass,
+                                      String trustStorePass) {
+        boolean isValid = false;
+        if (trustStorePass != null && !"".equals(trustStorePass)) {
+            if (log.isDebugEnabled()) {
+                log.debug("Trust Store Password cannot be found.");
+            }
+            isValid = true;
+        } else {
+            if (identityStorePass != null && !"".equals(identityStorePass) &&
+                    identityKeyPass != null && !"".equals(identityKeyPass)) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Identity Store Password "
+                                      + "and Identity Store private key Password cannot be found.");
+                }
+                isValid = true;
+            }
+        }
+        return isValid;
+    }
+
+    /**
+     * Initialize the Cipher decryption provider.
+     */
+    private void initCipherDecryptProvider() {
+        if (decryptionProvider != null) return;
+        Properties properties = loadProperties();
+
+        // Load algorithm
+        String algorithm = getCipherTransformation(properties);
+
+        // Load keyStore
+        String keyStore = MiscellaneousUtil.getProperty(properties,
+                                                        SecureVaultConstants.DOT + SecureVaultConstants.ALGORITHM,
+                                                        null);
+
+        KeyStoreWrapper keyStoreWrapper;
+
+        if (SecureVaultConstants.TRUSTED.equals(keyStore)) {
+            keyStoreWrapper = trustKeyStoreWrapper;
+
+        } else {
+            keyStoreWrapper = identityKeyStoreWrapper;
+        }
+
+        CipherInformation cipherInformation = new CipherInformation();
+        cipherInformation.setAlgorithm(algorithm);
+        cipherInformation.setCipherOperationMode(CipherOperationMode.DECRYPT);
+        cipherInformation.setInType(EncodingType.BASE64); // TODO
+        decryptionProvider = CipherFactory.createCipher(cipherInformation, keyStoreWrapper);
+    }
+
+    /**
+     * Get the Cipher Transformation to be used by the Cipher. We have the option of configuring this globally as a
+     * System Property '-Dorg.wso2.CipherTransformation', which can be overridden at the 'secret-conf.properties' level
+     * by specifying the property 'keystore.identity.CipherTransformation'. If neither are configured the default 'RSA'
+     * will be used
+     *
+     * @param properties Properties from the 'secret-conf.properties' file
+     * @return Cipher Transformation String
+     */
+    private String getCipherTransformation(Properties properties) {
+        String cipherTransformation = System.getProperty(SecureVaultConstants.CIPHER_TRANSFORMATION_SYSTEM_PROPERTY);
+
+        if (cipherTransformation == null) {
+            cipherTransformation = SecureVaultConstants.DEFAULT_ALGORITHM;
+        }
+
+        return MiscellaneousUtil.getProperty(properties,
+                                             SecureVaultConstants.CIPHER_TRANSFORMATION_SECRET_CONF_PROPERTY,
+                                             cipherTransformation);
+    }
+
+    /**
+     * Read the secret-conf.properties file located at conf/security directory and load the properties.
+     *
+     * @return properties in the secret-conf.properties file
+     */
+    private static Properties loadProperties() {
+        Properties properties = new Properties();
+        String confPath = System.getProperty(SecureVaultConstants.CONF_LOCATION);
+        if (confPath == null) {
+            confPath = Paths.get("repository", "conf").toString();
+        }
+        String filePath = Paths.get(confPath, SecureVaultConstants.SECURITY_DIR, SecureVaultConstants.SECRET_CONF)
+                .toString();
+
+        File dataSourceFile = new File(filePath);
+        if (!dataSourceFile.exists()) {
+            return properties;
+        }
+
+        try (InputStream in = new FileInputStream(dataSourceFile)) {
+            properties.load(in);
+        } catch (IOException e) {
+            String msg = "Error loading properties from a file at :" + filePath;
+            log.warn(msg, e);
+            return properties;
+        }
+        return properties;
+    }
+
+    DecryptionProvider getDecryptionProvider() {
+        return decryptionProvider;
+    }
+
+    private static void handleException(String msg) {
+        //throw new CipherToolException(msg);
+    }
+}
---
+++ components/data-services/org.wso2.carbon.dataservices.core/src/main/java/org/wso2/carbon/dataservices/core/security/SecureVaultResolver.java
@@ -0,0 +1,105 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
+package org.wso2.carbon.dataservices.core.security;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.dataservices.core.internal.DataServicesDSComponent;
+import org.wso2.carbon.registry.core.Registry;
+import org.wso2.carbon.registry.core.Resource;
+import org.wso2.carbon.registry.core.exceptions.RegistryException;
+import org.wso2.carbon.registry.core.service.RegistryService;
+import org.wso2.securevault.DecryptionProvider;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Check the secret alias match with the vaultLookupPattern regex.
+ */
+public class SecureVaultResolver {
+    private static Log log = LogFactory.getLog(SecureVaultResolver.class);
+
+    private SecureVaultResolver() {
+    }
+
+    /**
+     * Regex for secure vault expression.
+     */
+    private static final String SECURE_VAULT_REGEX = "\\{(wso2:vault-lookup\\('(.*?)'\\))\\}";
+    private static final String SECURE_VAULT_LOOKUP_PLACEHOLDER_PREFIX = "{wso2:vault-lookup('";
+    private static final String SECURE_VAULT_LOOKUP_PLACEHOLDER_SUFFIX = "')}";
+
+    private static Pattern vaultLookupPattern = Pattern.compile(SECURE_VAULT_REGEX);
+
+    /**
+     * Check the secret alias match with the vaultLookupPattern regex.
+     *
+     * @param text Text of the parameter
+     * @return boolean state of the pattern existence
+     */
+    public static boolean checkVaultLookupPattersExists(String text) {
+        Matcher lookupMatcher = vaultLookupPattern.matcher(text);
+        return lookupMatcher.find();
+    }
+
+    /**
+     * Resolve the secret from registry.
+     *
+     * @param value secure-vault expression in the format of {wso2:vault-lookup('?')}
+     * @return resolved secret
+     */
+    public static String resolve(String value) throws RegistryException {
+        // Get the actual alias from the vault-lookup expression
+        String secretAlias = StringUtils.substringBetween(value, SECURE_VAULT_LOOKUP_PLACEHOLDER_PREFIX,
+                                                          SECURE_VAULT_LOOKUP_PLACEHOLDER_SUFFIX);
+        return getSecret(secretAlias);
+    }
+
+    /**
+     * Get secret the actual password from the alias using the cipher decryption provider.
+     *
+     * @param alias alias of the actual password
+     * @return the actual password from the Secure Vault Password Management if exists, otherwise alias itself
+     */
+    private static String getSecret(String alias) throws RegistryException {
+        // Get config system registry from the registry service
+        RegistryService registryService = DataServicesDSComponent.getRegistryService();
+        Registry registry = registryService.getConfigSystemRegistry();
+
+        String encryptedValue = null;
+        if (registry != null && registry.resourceExists(SecureVaultConstants.ENCRYPTED_PROPERTY_STORAGE_PATH)) {
+            Resource registryResource = registry.get(SecureVaultConstants.ENCRYPTED_PROPERTY_STORAGE_PATH);
+            encryptedValue = registryResource.getProperty(alias);
+        }
+        if (encryptedValue == null) {
+            return alias;
+        }
+
+        DecryptionProvider decryptionProvider = CipherInitializer.getInstance().getDecryptionProvider();
+
+        if (decryptionProvider == null) {
+            log.error("Can not proceed decryption due to the secret repository initialization error");
+            return alias;
+        }
+        return new String(decryptionProvider.decrypt(encryptedValue.trim().getBytes()));
+    }
+}
